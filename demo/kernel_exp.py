55a8144ef7b8a3b4de3088e5b6f1b778ac62f174
teki97 <teki97@whu.edu.cn>
2020-07-29 23:33:22 +0800

    second commit

--- demo/kernel_exp.py
+++ demo/kernel_exp.py
@@ -18,14 +18,8 @@ def bat(filename,model,alpha,snr):
     sig1 = sig1.flatten()
     length = 400
     alpha = alpha
-    sig = sig1[0:length]
-
-
     sig = (sig1[0:length])/(max(sig1))
-    sig = add_noise(sig, snr)
-
-
-
+    
 
     ana_sig = analytic_x(sig)
     wvd = tfrwv(ana_sig)
@@ -71,67 +65,7 @@ def bat(filename,model,alpha,snr):
     # plt.axis('off')
     plt.savefig(path + '/fig2.png', bbox_inches='tight')
 
-
-
-
     io.savemat(path + '/bat' + str(snr) + '.mat', {'bat' + str(snr): np.mat(abs(smooth_wvd))})
-
-
-def eeg(filename,model,alpha):
-    eeg = io.loadmat('data/EEG_examples.mat')
-    sig1 = eeg['sig_seiz']
-    length = 256
-    sig = sig1.flatten()
-    alpha = alpha
-    sig = (sig[0:length])/(max(sig))
-    # ideal, wvd_t = gen_tfd_samples(num=2, length=length)
-
-
-    ana_sig = analytic_x(sig)
-    io.savemat('results/' + filename + '/eeg_example.mat', {'sig': np.mat(ana_sig)})
-    wvd = tfrwv(ana_sig)
-    wvd = wvd.flatten(order='F')
-    wvd = wvd.astype(np.float32)
-    # wvd = wvd*(max(wvd_t[0,:]-min(wvd_t[0,:]))/(max(wvd)-min(wvd)))
-    wvd = wvd*alpha
-    wvd = torch.tensor(wvd).to('cuda:0')
-    wvd = wvd.view(1, 1, length, length).transpose(3, 2)
-
-    # model = models.Encoder_2D_dilation(kernel_size=5,dilation=4)
-    # model = models.Encoder_2D_dilation(kernel_size=1, dilation=1)
-    # model.load_state_dict(torch.load('trained/' + filename + '.pth'))
-    model = model.cuda()
-
-    model.train(False)
-    model.eval()
-
-    smooth_wvd = model(wvd)
-    smooth_wvd = smooth_wvd.cpu()
-    smooth_wvd = smooth_wvd.data.numpy()
-    wvd = wvd.cpu()
-    wvd = wvd.data.numpy()
-
-    dyn = 15
-    plt.subplot(1, 2, 1)
-    imageTF(smooth_wvd, dyn)
-    plt.title('smooth wvd')
-    plt.subplot(1, 2, 2)
-    imageTF(wvd, dyn)
-    plt.title('wvd')
-
-    plt.savefig('results/' + filename + '/eeg_1.eps', bbox_inches='tight')
-
-    plt.figure()
-    plt.imshow(np.array(np.flipud(np.mat(abs(wvd)))))
-    # plt.axis('off')
-    plt.savefig('results/' + filename + '/eeg_fig1.png', bbox_inches='tight')
-
-    plt.figure()
-    plt.imshow(np.array(np.flipud(np.mat(abs(smooth_wvd)))))
-    # plt.axis('off')
-    plt.savefig('results/' + filename + '/eeg_fig2.png', bbox_inches='tight')
-
-    io.savemat('results/' + filename + '/eeg.mat', {'eeg': np.mat(abs(smooth_wvd))})
 
 
 def test_synthetic_paper(filename,model,snr):
@@ -269,60 +203,14 @@ def m_renyi(model, filename, noise):
             
         mc_renyi[0,s] = mc_renyi[0,s] / 100
     io.savemat('results/' + filename + '/ldk_r.mat',{'r_KLD' : np.mat(abs(mc_renyi))})
-    
-
-def m_renyi_EEG(model, filename, noise):
-    mc_max = 100
-    mc_renyi = np.zeros((1,len(noise)))
-
-    bat = io.loadmat('data/bat1.mat')
-    sig1 = bat['bat1']
-    sig1 = sig1.flatten()
-    length = 400
-    alpha = 1
-    # sig = sig1[0:length]
-
-
-
-    for s in range(len(noise)):
-        for mm in range(mc_max):
-
-            sig = (sig1[0:length]) / (max(sig1))
-            sig = add_noise(sig, noise[s])
-
-            ana_sig = analytic_x(sig)
-            wvd = tfrwv(ana_sig)
-            wvd = wvd.flatten(order='F')
-            wvd = wvd.astype(np.float32)
-            
-            wvd = wvd * alpha
-            wvd = torch.tensor(wvd).to('cuda:0')
-            wvd = wvd.view(1, 1, length, length).transpose(3, 2)
-
-            model.train(False)
-            model.eval()
-
-            with torch.no_grad():
-                smooth_wvd = model(wvd)
-            smooth_wvd = smooth_wvd.cpu()
-            smooth_wvd = smooth_wvd.data.numpy()
-            smooth_wvd = abs(smooth_wvd) / np.max(abs(smooth_wvd))
-            
-            test = renyi(smooth_wvd[0, 0, :, :])
-            mc_renyi[0,s] = mc_renyi[0,s] + test
-            
-        mc_renyi[0,s] = mc_renyi[0,s] / 100
-    io.savemat('results/' + filename + '/ldk_r.mat',{'r_KLD' : np.mat(abs(mc_renyi))})
 
 
 if __name__ == '__main__':
     filename1 = 'kernel_learning_1'
     filename2 = 'kernel_learning_2'
-    filename3 = 'kernel_learning_3'
     
     model1 = models.Encoder_2D_dilation_se_res_new(kernel_size=3, dilation=1)
-    model2 = models.Encoder_2D_dilation_se_res_new(kernel_size=3, dilation=1)
-    model3 = models.Encoder_2D_dilation_se_res_new(kernel_size=3, dilation=1)
+    model2 = models.Encoder_2D_dilation_se_res_new8(kernel_size=3, dilation=1)
     
     model1.load_state_dict(torch.load('trained/' + filename1 + '.pth'))
     model1 = model1.cuda()
@@ -330,10 +218,6 @@ if __name__ == '__main__':
     model2.load_state_dict(torch.load('trained/' + filename2 + '.pth'))
     model2 = model2.cuda()
 
-    model3.load_state_dict(torch.load('trained/' + filename3 + '.pth'))
-    model3 = model3.cuda()
-
     bat(filename1, model1, 1, 20)
     test_synthetic_paper(filename2, model2, 20)
-    eeg(filename3, model3, 1)
     
--- demo/models.py
+++ demo/models.py
@@ -110,3 +110,108 @@ class Encoder_2D_dilation_se_res_new(nn.Module):
         return out
 
 
+class Encoder_2D_dilation_se_res_new8(nn.Module):
+    def __init__(self,kernel_size,dilation):
+        super(Encoder_2D_dilation_se_res_new8, self).__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(1, 4, kernel_size=11, stride=1,dilation=dilation, padding=int(dilation*(11 - 1) / 2)),
+            nn.BatchNorm2d(4))
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(4, 8, kernel_size=9, stride=1, dilation=2 * dilation,
+                      padding=int(2 * dilation * (9 - 1) / 2)),
+            nn.BatchNorm2d(8))
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(8, 16, kernel_size=9, stride=1, dilation=4 * dilation,
+                      padding=int(4 * dilation * (9 - 1) / 2)),
+            nn.BatchNorm2d(16))
+        self.conv4 = nn.Sequential(
+            nn.Conv2d(16, 32, kernel_size=9, stride=1, dilation=8 * dilation,
+                      padding=int(8 * dilation * (9 - 1) / 2)),
+            nn.BatchNorm2d(32))
+        self.conv5 = nn.Sequential(
+            nn.Conv2d(32, 64, kernel_size=9, stride=1, dilation=16 * dilation,
+                      padding=int(16 * dilation * (9 - 1) / 2)),
+            nn.BatchNorm2d(64))
+        self.conv6 = nn.Sequential(
+            nn.Conv2d(64, 128, kernel_size=kernel_size, stride=1, dilation=32 * dilation,
+                      padding=int(32 * dilation * (kernel_size - 1) / 2)),
+            nn.BatchNorm2d(128))
+        self.conv7 = nn.Sequential(
+            nn.Conv2d(128, 128, kernel_size=kernel_size, stride=1, dilation=32 * dilation,
+                      padding=int(32 * dilation * (kernel_size - 1) / 2)),
+            nn.BatchNorm2d(128))
+        self.conv8 = nn.Sequential(
+            nn.Conv2d(128, 128, kernel_size=kernel_size, stride=1, dilation=32 * dilation,
+                      padding=int(32 * dilation * (kernel_size - 1) / 2)),
+            nn.BatchNorm2d(128))
+
+        self.res4 = nn.Conv2d(32, 64, kernel_size=1)
+        self.res5 = nn.Conv2d(64, 128, kernel_size=1)
+        self.res6 = nn.Conv2d(128, 128, kernel_size=1)
+        self.res7 = nn.Conv2d(128, 128, kernel_size=1)
+
+        self.se1 = SELayer(4, 1)
+        self.se2 = SELayer(8, 1)
+        self.se3 = SELayer(16, 1)
+        self.se4 = SELayer(32, 1)
+        self.se41 = SELayer(64, 64)
+        self.se42 = SELayer(64, 64)
+        self.se51 = SELayer(128, 128)
+        self.se52 = SELayer(128, 128)
+        self.se61 = SELayer(128, 128)
+        self.se62 = SELayer(128, 128)
+        self.se71 = SELayer(128, 128)
+        self.se72 = SELayer(128, 128)
+
+        self.relu = nn.ReLU(inplace=True)
+        self.conv1x1 = nn.Conv2d(128,1, kernel_size=1)
+
+    def forward(self, x):
+        out1 = self.conv1(x)
+        out1 = self.relu(out1)
+        out1 = self.se1(out1)
+
+        out2 = self.conv2(out1)
+        out2 = self.relu(out2)
+        out2 = self.se2(out2)
+
+        out3 = self.conv3(out2)
+        out3 = self.relu(out3)
+        out3 = self.se3(out3)
+
+        out4 = self.conv4(out3)
+        out4 = self.relu(out4)
+        out4 = self.se4(out4)
+
+        out44 = self.res4(out4)
+        out44 = self.se41(out44)
+        out5 = self.conv5(out4)
+        out5 = self.se42(out5)
+        out5 = out5 + out44
+        out5 = self.relu(out5)
+
+        out55 = self.res5(out5)
+        out55 = self.se51(out55)
+        out6 = self.conv6(out5)
+        out6 = self.se52(out6)
+        out6 = out6 + out55
+        out6 = self.relu(out6)
+
+        out66 = self.res6(out6)
+        out66 = self.se61(out66)
+        out7 = self.conv7(out6)
+        out7 = self.se61(out7)
+        out7 = out7 + out66
+        out7 = self.relu(out7)
+
+        out77 = self.res7(out7)
+        out77 = self.se71(out77)
+        out8 = self.conv8(out7)
+        out8 = self.se72(out8)
+        out8 = out8 + out77
+        out8 = self.relu(out8)
+        
+        out = self.conv1x1(out8)
+
+        return out
+
